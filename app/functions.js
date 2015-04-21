3ffd2e3147cbf20939ab9f7db82c9538cfa775f4
Rafael Freire <rafaelcfreire@gmail.com>
2015-04-21 00:01:48 -0300

    Function implementations

--- app/functions.js
+++ app/functions.js
@@ -3,11 +3,11 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn(arr[0], arr[1], arr[2]);
     },
 
     speak : function(fn, obj) {
-
+        return fn.apply(obj);
     },
 
     functionFunction : function(str) {
