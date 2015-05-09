61a5d0fc6cc2e3e510d93102431b30594189cf63
Rafael Freire <rafaelcfreire@gmail.com>
2015-05-09 11:54:16 -0300

    Numbers assesments

--- app/numbers.js
+++ app/numbers.js
@@ -7,11 +7,28 @@ define(function() {
     },
 
     base10: function(str) {
+        var weight = 1;
+        var sum = 0;
+        for(var a = str.length - 1; a >= 0; a--){
+            sum += str.charAt(a) * weight;
+            weight *= 2;
+        }
 
+        return sum;
     },
 
     convertToBinary: function(num) {
-
+        var intNumber = parseInt(num);
+        var finalArray = "";
+        do {
+            var currentValue = parseInt(intNumber % 2);
+            finalArray +=currentValue;
+            intNumber = intNumber / 2;
+        } while(parseInt(intNumber) > parseInt(0))
+        for(var a = finalArray.length; a < 8; a++){
+            finalArray = finalArray.concat("0");
+        }
+        return finalArray.split("").reverse().join("");
     },
 
     multiply: function(a, b) {
