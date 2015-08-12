9edd65cd8475ecb4bf9c5fa60cf99f6a32ffb9d2
Rafael Freire <rafaelcfreire@gmail.com>
2015-08-12 12:16:35 -0300

    Implementing regex test

--- app/regex.js
+++ app/regex.js
@@ -3,7 +3,10 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        if (str.match(/\d/) )
+            return true;
+        else
+            return false;
     },
 
     containsRepeatingLetter : function(str) {
