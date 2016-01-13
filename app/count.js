332828612f420c3456ebca5f55991226657134de
Rafael Freire <rafaelcfreire@gmail.com>
2016-01-13 17:31:36 -0200

    Implementing counting function

--- app/count.js
+++ app/count.js
@@ -3,6 +3,13 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+    	setInterval(function(){
+    		console.log(
+    			for(start < end; start ++) {
+    				start;
+    			}
+    		)
+    	}, 100);
     }
   };
 });
